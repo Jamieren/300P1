5b2dd466c3eda8892f661544ccb0770ea23dd809
Zionate7 <natedorazio@gmail.com>
2023-11-04 22:49:21 -0600

    added names and ucid to start session

--- com.thelocalmarketplace.software/startSession.java
+++ com.thelocalmarketplace.software/startSession.java
@@ -1,3 +1,9 @@
+//Daniel Adebisi UCID: 30179418
+//Adrian Aure UCID: 30164498
+//Nathaniel D'Orazio UCID: 30118713
+//Arian Popal UCID: 30189384
+//Zhenhui Ren UCID: 30139966
+
 
 public class startSession {
 
