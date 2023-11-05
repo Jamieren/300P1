cfaa4b9d3e3df904592275c64fe3a03e0c7617ae
dadebisi17 <149823916+dadebisi17@users.noreply.github.com>
2023-11-04 18:54:19 -0600

    Update weightDiscrepancy.java

--- weightDiscrepancy.java
+++ weightDiscrepancy.java
@@ -1,13 +1,1 @@
-package com.thelocalmarketplace.software;
 
-import com.jjjwelectronics.scale.ElectronicScale;
-import java.math.BigDecimal;
-
-BigDecimal currentWeight = electronicScale.getCurrentMassOnTheScale().inGrams();
-BigDecimal expectedWeight = BigDecimal.valueOf(product.getExpectedWeight());
-
-public class weightDiscrepancy{
-  public weightChanged(){
-  if()
-  }
-}
